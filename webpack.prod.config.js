b89bf12aaa0f11fba8e392d5fa849bfe217e4454
disgraceful <noolic97@gmail.com>
2020-05-12 15:47:05 +0300

    Rebuild

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./js/main.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: "prod.js",
     publicPath: "./assets/",
   },
 };
